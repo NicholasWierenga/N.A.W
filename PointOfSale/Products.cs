d088299749221a4b8bf5473f921819edc367c21d
Nicholas Wierenga <NicholasWierenga@gmail.com>
2022-04-27 17:04:59 -0400

    initial commit

---
+++ PointOfSale/Products.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale
+{
+    public enum Category
+    {
+        Entree,
+        Side,
+        Drink,
+    }
+    class Products
+    {
+
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public string Desc { get; set; }
+        public double Price { get; set; }
+
+        public Products(string Name, Category Category, string Desc, double Price)
+        {
+            this.Name = Name;
+            this.Category = Category;
+            this.Desc = Desc;
+            this.Price = Price;
+        }
+
+        public virtual void PrintItem()
+        {
+            Console.WriteLine(Name + " for " + "$" + Price + ".");
+        }
+    }
+}
---
+++ PointOfSale/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PointOfSale
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to N.A.W.");
+
+            Items items = new Items();
+
+            items.CheckOut();
+
+
+        }
+    }
+}
