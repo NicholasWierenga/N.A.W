21bb1f1c8425f29f43ef44017d193b76a629b240
WendyJackson1 <wendyrobinh@gmail.com>
2022-05-02 13:08:00 -0400

    starting text file

--- PointOfSale/Items.cs
+++ PointOfSale/Items.cs
@@ -6,119 +6,109 @@ using System.Threading.Tasks;
 
 namespace PointOfSale
 {
-    public class Items
-    {
-        List<Products> allItems = new List<Products>
-        {
-            new Products("Burger", Category.Entree, "It's a cheeseburger", 5.00),
-            new Products("Nuggets", Category.Entree, "8 chicken nuggets", 4.50),
-            new Products("Taco", Category.Entree, "A taco", 3.25),
-            new Products("Hot dog", Category.Entree, "A hot dog", 2.00),
-            new Products("Fries", Category.Side, "Curly fries", 1.50),
-            new Products("Tots", Category.Side, "Tater tots", 3.25),
-            new Products("Onion Rings", Category.Side, "6 big rings", 2.50),
-            new Products("Cole Slaw", Category.Side, "It's cole slaw", 1.25),
-            new Products("Lemonade", Category.Drink, "Sour drink", 5.00),
-            new Products("Coke", Category.Drink, "Coca-cola", 2.00),
-            new Products("Water", Category.Drink, "Plain h2o", 0.99),
-            new Products("Slush", Category.Drink, "Cold drink", 5.00),
-        };
-        List<string> orderDetails = new List<string>();
-        int padLength = 27; // to adjust to change spacing between order details and amounts.
-        int index;
+//    public class Items
+//    {
 
-        public void CheckOut()
-        {
-            double subTotal = 0;
-            double salesTax;
-            double total;
 
-            do
-            {
-                PrintAll();
-                
-                index = GetUserOrder();
 
-                int amountOrdered = Helper.GetIntInput("How many do you want to order?");
 
-                Console.WriteLine();
-                
-                subTotal = subTotal + amountOrdered * allItems[index].Price;
-                salesTax = 0.06 * subTotal;
-                total = subTotal + salesTax;
+//    List<string> orderDetails = new List<string>();
+//    int padLength = 27; // to adjust to change spacing between order details and amounts.
+//    int index;
+//    }
 
-                orderDetails.Add((amountOrdered + "x " + allItems[index].Name).PadRight(padLength) 
-                    + "$" + (amountOrdered * allItems[index].Price).ToString("0.00") + "."); // Adds a string with amount ordered, item's name, and price for it.
+//    public void CheckOut()
+//    {
+//        double subTotal = 0;
+//        double salesTax;
+//        double total;
 
-                PrintCurrentOrder(subTotal, salesTax, total);
+//        do
+//        {
+//            PrintAll();
 
-                Console.WriteLine();
+//            index = GetUserOrder();
 
-            } while (Helper.RunAgain("Would you like to order anything else today? y/n"));
+//            int amountOrdered = Helper.GetIntInput("How many do you want to order?");
 
-            CompleteOrder(subTotal, salesTax, total);
-        }
+//            Console.WriteLine();
 
-        public void PrintAll()
-        {
-            for (int i = 0; i < allItems.Count; i++)
-            {
-                Console.WriteLine(((i + 1) + "  " + allItems[i].Name + " ").PadRight(25, '.') + " Price: $" + allItems[i].Price.ToString("0.00") + ".");
-                Console.WriteLine("(".PadLeft(12) + allItems[i].Desc + ")  ");
-            }
-        }
+//            subTotal = subTotal + amountOrdered * allItems[index].Price;
+//            salesTax = 0.06 * subTotal;
+//            total = subTotal + salesTax;
 
-        public int GetUserOrder()
-        {
-            string order = Helper.GetStringInput("What item from the menu would you like to order? Enter its number or name.");
-            index = allItems.FindIndex(item => item.Name.ToLower() == order); // We check if the order is in the list, equals -1 if it isn't.
+//            orderDetails.Add((amountOrdered + "x " + allItems[index].Name).PadRight(padLength)
+//                + "$" + (amountOrdered * allItems[index].Price).ToString("0.00") + "."); // Adds a string with amount ordered, item's name, and price for it.
 
-            if (index != -1) // For when user entered name.
-            {
-                Console.WriteLine("You've selected: ");
-                Console.WriteLine(allItems[index].Name + " for " + "$" + allItems[index].Price.ToString("0.00") + ".");
-                return index;
-            }
-            else if (int.TryParse(order, out index) && index >= 1 && index <= allItems.Count) // For when user entered number.
-            {
-                Console.WriteLine("You've selected: ");
-                Console.WriteLine(allItems[index - 1].Name + " for " + "$" + allItems[index - 1].Price.ToString("0.00") + ".");
-                return index - 1; // Since we listed off the items starting at 1 and not 0, we have to return whatever they put - 1.
-            }
-            else
-            {
-                PrintAll();
-                Console.WriteLine("We do not have that item. Let's try again.");
-                return GetUserOrder();
-            }
-        }
+//            PrintCurrentOrder(subTotal, salesTax, total);
 
-        public void PrintCurrentOrder(double subPrice, double salesTax, double total)
-        {
-            for (int i = 0; i < orderDetails.Count; i++)
-            {
-                Console.WriteLine(orderDetails[i]);
-            }
+//            Console.WriteLine();
 
-            Console.WriteLine("Subtotal ".PadRight(padLength) + "$" + subPrice.ToString("0.00") + ".");
-            Console.WriteLine("Sales tax ".PadRight(padLength) + "$" + salesTax.ToString("0.00") + ".");
-            Console.WriteLine("".PadLeft(padLength + ("$" + total.ToString("0.00") + ".").Length, '_'));
-            Console.WriteLine("Grand total ".PadRight(padLength) + "$" + total.ToString("0.00") + ".");
-        }
+//        } while (Helper.RunAgain("Would you like to order anything else today? y/n"));
 
-        public void CompleteOrder(double subPrice, double salesTax, double total)
-        {
-            Payment customerPay = new Payment(Math.Round(total, 2), padLength); // We do round, because total might have more than 2 decimals.
-            string payMessage = customerPay.Pay();
+//        CompleteOrder(subTotal, salesTax, total);
+//    }
 
-            int receiptLength = orderDetails.Max(str => str.Length); // Used to print the ===RECEIPT=== line
-            Console.WriteLine("RECEIPT".PadLeft(receiptLength / 2 + 4, '=').PadRight(receiptLength, '=')); 
+//    public void PrintAll()
+//    {
+//        for (int i = 0; i < allItems.Count; i++)
+//        {
+//            Console.WriteLine(((i + 1) + "  " + allItems[i].Name + " ").PadRight(25, '.') + " Price: $" + allItems[i].Price.ToString("0.00") + ".");
+//            Console.WriteLine("(".PadLeft(12) + allItems[i].Desc + ")  ");
+//        }
+//    }
 
-            PrintCurrentOrder(subPrice, salesTax, total);
+//    public int GetUserOrder()
+//    {
+//        string order = Helper.GetStringInput("What item from the menu would you like to order? Enter its number or name.");
+//        index = allItems.FindIndex(item => item.Name.ToLower() == order); // We check if the order is in the list, equals -1 if it isn't.
 
-            Console.WriteLine(payMessage);
-            Console.WriteLine("Thank you for your business. Please come again!");
-        }
-    }
+//        if (index != -1) // For when user entered name.
+//        {
+//            Console.WriteLine("You've selected: ");
+//            Console.WriteLine(allItems[index].Name + " for " + "$" + allItems[index].Price.ToString("0.00") + ".");
+//            return index;
+//        }
+//        else if (int.TryParse(order, out index) && index >= 1 && index <= allItems.Count) // For when user entered number.
+//        {
+//            Console.WriteLine("You've selected: ");
+//            Console.WriteLine(allItems[index - 1].Name + " for " + "$" + allItems[index - 1].Price.ToString("0.00") + ".");
+//            return index - 1; // Since we listed off the items starting at 1 and not 0, we have to return whatever they put - 1.
+//        }
+//        else
+//        {
+//            PrintAll();
+//            Console.WriteLine("We do not have that item. Let's try again.");
+//            return GetUserOrder();
+//        }
+//    }
+
+//    public void PrintCurrentOrder(double subPrice, double salesTax, double total)
+//    {
+//        for (int i = 0; i < orderDetails.Count; i++)
+//        {
+//            Console.WriteLine(orderDetails[i]);
+//        }
+
+//        Console.WriteLine("Subtotal ".PadRight(padLength) + "$" + subPrice.ToString("0.00") + ".");
+//        Console.WriteLine("Sales tax ".PadRight(padLength) + "$" + salesTax.ToString("0.00") + ".");
+//        Console.WriteLine("".PadLeft(padLength + ("$" + total.ToString("0.00") + ".").Length, '_'));
+//        Console.WriteLine("Grand total ".PadRight(padLength) + "$" + total.ToString("0.00") + ".");
+//    }
+
+//    public void CompleteOrder(double subPrice, double salesTax, double total)
+//    {
+//        Payment customerPay = new Payment(Math.Round(total, 2), padLength); // We do round, because total might have more than 2 decimals.
+//        string payMessage = customerPay.Pay();
+
+//        int receiptLength = orderDetails.Max(str => str.Length); // Used to print the ===RECEIPT=== line
+//        Console.WriteLine("RECEIPT".PadLeft(receiptLength / 2 + 4, '=').PadRight(receiptLength, '='));
+
+//        PrintCurrentOrder(subPrice, salesTax, total);
+
+//        Console.WriteLine(payMessage);
+//        Console.WriteLine("Thank you for your business. Please come again!");
+//    }
+
 }
 
--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -2,21 +2,44 @@
 {
     public class Program
     {
-        
+
         public static void Main()
         {
+            string filePath = @"C:\Users\wendy\OneDrive\Desktop\naw project\N.A.W\AllItems.txt";
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+                //string[] things = line.Spilt(",");
+                //Items I = new Items(Items[1], Items[2], Items[3], Items[4]);
+                //allItems.Add(I);
+            }
+            File.WriteAllLines(filePath, lines);
+            List<string> outContents = new List<string>();
+            foreach (string line in lines)
+            {
+
+                outContents.Add(line.ToString());
+            }
+            string outFile = @"C:\Users\wendy\OneDrive\Desktop\naw project\N.A.W\outFile.txt";
+            File.WriteAllLines(outFile, outContents);
+
+
+
             Console.WriteLine();
             Console.WriteLine("              Welcome to N.A.W.");
             Console.WriteLine("Take a moment to look over the menu and I will take your order!");
 
-                do
-                {
-                    Items items = new Items();
-                    items.CheckOut();
+            //    do
+            //    {
+            //        Items items = new Items();
+            //        items.CheckOut();
 
-                } while (Helper.RunAgain("Would you like to run the program again? y/n"));
+            //    } while (Helper.RunAgain("Would you like to run the program again? y/n"));
 
-            Console.WriteLine("Goodbye.");
+            //Console.WriteLine("Goodbye.");
         }
         
     }
